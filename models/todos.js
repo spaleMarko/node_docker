39fb8eeb516886be04c97fcc669b61ae46807260
spaleMarko <spasicmarko2010@hotmail.com>
2020-11-12 16:12:04 +0100

    #5 Beggin start files

--- models/todos.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const todosSchema = new mongoose.Schema({
-    description: String,
-    complete: Boolean
-});
-
-module.exports.Todos = mongoose.model('Todos', todosSchema);
