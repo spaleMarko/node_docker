93e063742a8bd1acc5e5e4cf65d68b86df094d4f
spaleMarko <spasicmarko2010@hotmail.com>
2020-11-13 00:41:09 +0100

    #5 6. Added

--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs');
 const User = require('../model/User');
 
 //VALIDATION
@@ -8,17 +9,23 @@ router.post('/register', async (req, res) => {
     // LETS VALIDATE THE DATA BEFORE WE A USER
     const {error} = registerValidation(req.body);
     if (error) return res.status(400).send(error.details[0].message)
+
     //Checking if the user already in the database
     const emailExist = await User.findOne({email: req.body.email})
     if (emailExist) return res.status(400).send('Email alredy exists')
+
+    // Hash password
+    const salt = await bcrypt.genSalt(12);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
     // Create new User
     const user = new User({
         email: req.body.email,
-        password: req.body.password
+        password: hashedPassword
     });
     try{
         const savedUser = await user.save();
-        res.send(savedUser);
+        res.send({user: user._id});
     }catch(err){
         res.status(400).send(err);
     }
